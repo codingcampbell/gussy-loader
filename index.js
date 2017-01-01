489d6827b6788268762e735ce70ca056ea03ccf9
Matt Campbell <matt@mattcampbell.net>
2017-01-01 15:27:19 -0500

    Add style directory to watched dependencies

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ var gussy = require('gussy');
 var utils = require('loader-utils');
 
 module.exports = function(source) {
-  delete require.cache[this.resourcePath];
+  var dir = this.resourcePath.split(/\//).slice(0, -1).join('/');
+  var dirRegex = new RegExp('^' + dir);
+  this.addContextDependency(dir);
+
+  Object.keys(require.cache).forEach(function(key) {
+    if (dirRegex.test(key)) {
+      delete require.cache[key];
+    }
+  });
+
   gussy(utils.parseQuery(this.query)).compile(require(this.resourcePath), this.async().bind(this, null));
 };
